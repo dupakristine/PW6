66cb139e2e597d2b4775d1a60cf4981befd6e1c9
dupakristine <dupakristine96@gmail.com>
2022-09-26 19:10:07 +0800

    first commit

---
+++ resources/views/Personal Info.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim-Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+   <div class="text-center"></div>
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link" >About</a>
+            <a href="{{url('/Personal Info')}}" class="nav-link">Personal Info</a>
+        </nav>
+        
+        <div class="text-center"></div>
+        <div class="card shadow-sm mx-auto" style="width:600px">
+             <div class="card-body">
+                 <div class="card-title"><stron></strong></div>
+                 <p>
+                 Name:<strong> Kristine Dupa</strong>
+                   A Third year BS in Information Technology student in Mater Dei College
+                 </p>   
+                 <p>
+                 Contact: 096387***** <br>
+                 email: dupakristine96@gmail.com<br>
+                 Address: Ilijan Norte, Tubigon, Bohol</br>
+
+                 </p>   
+            </div>
+        </div>
+    </div>
+    
+
+</body>
+</html>
---
+++ resources/views/about.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim-Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+   <div class="text-center"></div>
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link" >About</a>
+            <a href="{{url('/Personal Info')}}" class="nav-link">Personal Info</a>
+        </nav>
+        
+        <div class="text-center"></div>
+        <div class="card shadow-sm mx-auto" style="width:600px">
+             <div class="card-body">
+                 <div class="card-title"></div>
+                 <p>
+                 My Prelim Project is on this page and submitted to our very talented instructor <strong><em>Mr.Benjie Lenteria </em></strong>
+                 </p>   
+            </div>
+        </div>
+    </div>
+    
+
+</body>
+</html>
